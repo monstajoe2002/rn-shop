2503f80ab9a3c2224d87e05dcbe089e89e894d60
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2024-10-30 23:58:59 +0300

    build products details screen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,31 +1,33 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { Stack } from "expo-router";
-
+import { ToastProvider } from "react-native-toast-notifications";
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="(shop)"
-        options={{ headerShown: true, title: "Shop" }}
-      />
-      <Stack.Screen
-        name="categories"
-        options={{ headerShown: true, title: "Categories" }}
-      />
-      <Stack.Screen
-        name="product"
-        options={{ headerShown: true, title: "Product" }}
-      />
-      <Stack.Screen
-        name="cart"
-        options={{
-          headerShown: true,
-          title: "Shopping Cart",
-          presentation: "modal",
-        }}
-      />
-      <Stack.Screen name="auth" options={{ headerShown: true }} />
-    </Stack>
+    <ToastProvider>
+      <Stack>
+        <Stack.Screen
+          name="(shop)"
+          options={{ headerShown: true, title: "Shop" }}
+        />
+        <Stack.Screen
+          name="categories"
+          options={{ headerShown: true, title: "Categories" }}
+        />
+        <Stack.Screen
+          name="product"
+          options={{ headerShown: false, title: "Product" }}
+        />
+        <Stack.Screen
+          name="cart"
+          options={{
+            headerShown: true,
+            title: "Shopping Cart",
+            presentation: "modal",
+          }}
+        />
+        <Stack.Screen name="auth" options={{ headerShown: true }} />
+      </Stack>
+    </ToastProvider>
   );
 }
