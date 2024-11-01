816cde01310d5bddf1a30e1d09e3da5e6d42f59f
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2024-11-01 14:53:16 +0200

    check for user in the database

--- src/providers/auth-provider.tsx
+++ src/providers/auth-provider.tsx
@@ -31,18 +31,18 @@ export default function AuthProvider({ children }: PropsWithChildren) {
         data: { session },
       } = await supabase.auth.getSession();
       setSession(session);
-      // if (session) {
-      //   const { data: user, error } = await supabase
-      //     .from("users")
-      //     .select("*")
-      //     .eq("id", session.user.id)
-      //     .single();
-      //   if (error) {
-      //     console.error(error);
-      //   } else {
-      //     setUser(user);
-      //   }
-      // }
+      if (session) {
+        const { data: user, error } = await supabase
+          .from("users")
+          .select("*")
+          .eq("id", session.user.id)
+          .single();
+        if (error) {
+          console.error(error);
+        } else {
+          setUser(user);
+        }
+      }
       setMounting(false);
     };
     fetchSession();
